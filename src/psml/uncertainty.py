3a3dfc551e1313d67cd2126bd57677d16eff1249
Logan A. Burnett <loganb14@mit.edu>
2026-03-18 15:22:33 -0400

    Clean up legacy notebook prediction helpers

--- src/psml/uncertainty.py
+++
@@ -1,7 +0,0 @@
-from .predict import calculate_and_display_metrics, plot_predictions, predict_with_uncertainty
-
-__all__ = [
-    "calculate_and_display_metrics",
-    "plot_predictions",
-    "predict_with_uncertainty",
-]
